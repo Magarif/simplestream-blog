7bfb359fa052ee2befc42cbf0e7dbb5a68b88abb
Petar Popovic <petar.popovic@icloud.com>
2020-01-09 16:10:59 +0100

    [TASK] Define method for new comment creation

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Comment;
+use App\Http\Requests\CommentsFormRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CommentsController extends Controller
 {
-    //
+
+    /**
+     * Generate new comment
+     *
+     * @param CommentsFormRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function newComment(CommentsFormRequest $request)
+    {
+        $user = Auth::user();
+        $comment = new Comment([
+            'user_id' => $user->getAuthIdentifier(),
+            'post_id' => $request->get('post_id'),
+            'text' => $request->get('text')
+        ]);
+
+        $comment->save();
+        return redirect()->back()->with('status', 'Your comment has been created!');
+    }
 }
