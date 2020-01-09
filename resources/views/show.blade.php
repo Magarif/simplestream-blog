30decc6da16abbb3f95cb97eac443cef78e2b28a
Petar Popovic <petar.popovic@icloud.com>
2020-01-09 09:40:15 +0100

    [TASK] Add link to post edit page from detail post view

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -10,5 +10,10 @@
             <h2 class="header">{!! $post->title !!}</h2>
             <p>{!! $post->body !!}</p>
         </div>
+        <div class="container">
+            <div class="row">
+                <a href="{!! action('PostController@edit', $post->slug) !!}" class="btn btn-primary pull-left">Edit post</a>
+            </div>
+        </div>
     </div>
 @endsection
