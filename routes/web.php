9b3852d0fb7108e87cfd2b2ce31874a4b7eeb6a5
Petar Popovic <petar.popovic@icloud.com>
2020-01-09 10:45:14 +0100

    [TASK] Define login and logout routes, also define '/' route

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
 */
 
 
+Route::get('/', 'PostController@index');
 Route::get('/home', 'PostController@index');
 Route::get('/create', 'PostController@create');
 Route::post('/create', 'PostController@store');
@@ -22,3 +23,6 @@ Route::delete('/post/{slug?}/delete', 'PostController@destroy');
 // Auth routes
 Route::get('/auth/register', 'Auth\RegisterController@showRegistrationForm');
 Route::post('/auth/register', 'Auth\RegisterController@register');
+Route::get('/auth/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('/auth/login', 'Auth\LoginController@login');
+Route::get('/auth/logout', 'Auth\LoginController@logout');
