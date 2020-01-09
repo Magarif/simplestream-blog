0ea285069d96b8ae51a104c1716c822205ef5dbd
Petar Popovic <petar.popovic@icloud.com>
2020-01-09 15:42:12 +0100

    [TASK] Define User-Comment and User-Post relations

--- app/User.php
+++ app/User.php
@@ -36,4 +36,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts()
+    {
+        return $this->hasMany('App\Posts', 'user_id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comments', 'user_id');
+    }
 }
